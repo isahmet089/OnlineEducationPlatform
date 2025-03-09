a6a98b8671c88543359d09dfa4cd558d57ff4ecc
isahmet089 <isahmet089@gmail.com>
2025-03-09 23:20:09 +0300

    Add CORS support and error handling middleware; implement request logging and enhance logging functionality(CORS ERROR HANDLER AND LOGGER )

--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@ const express = require('express');
 const cookieParser = require('cookie-parser')
 const app = express();
 const morgan = require('morgan')
+const cors =require("cors");
+const corsOption = require("./config/corsConfig.js");
+const {logger} = require("./middleware/logEvents.js");
+const errorHandler = require("./middleware/errorHandler");
+
+
 //MİDDİLWARE
+app.use(cors(corsOption))
 app.use(express.json()); // JSON verisini parse eder.
 app.use(cookieParser());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
+app.use(logger);
+
+
 // ROUTES REQIRE
 const userRoutes =require("./routes/userRoutes");
 const authRoutes =require("./routes/authRoutes.js")
@@ -13,5 +23,7 @@ const authRoutes =require("./routes/authRoutes.js")
 //ROUTE 
 app.use("/api/users",userRoutes);
 app.use("/auth",authRoutes)
+
+app.use(errorHandler);
 
 module.exports=app;
