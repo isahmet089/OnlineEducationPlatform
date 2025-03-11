9081b07b43883578bb4decb8d46c6f88ca465891
isahmet089 <isahmet089@gmail.com>
2025-03-11 20:19:52 +0300

    Refactor course management functionality; update course routes and enhance course controller methods for better error handling and user authorization(course crud işlemleri)

--- src/controller/courseController.js
+++ src/controller/courseController.js
@@ -1,22 +1,61 @@
 const Course = require("../model/Course");
+const User = require("../model/User");
 
-const getCourse = async (req,res)=>{
+
+const getCourse = async (req, res) => {
     try {
         const courses = await Course.find()
-        if(!courses) return res.status(404).json({message : "Kurs bulunamadı!"});
-        res.status(200).json({message : {courses}});
+            .populate("category", "name") 
+            .populate("instructor", "name email"); 
+        if (courses.length === 0) {
+            return res.status(404).json({ message: "Kurs bulunamadı!" });
+        }
+        res.status(200).json({ courses });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+const addCourse =async (req,res)=>{
+    try {
+        const {title,description,category} = req.body;
+        const {id} = req.params;
+        const instructor = await User.findById(id)
+        if(!instructor) return res.status(400).json({message : "Buna yetkiniz yok."})
+        const addCourse = new Course({
+            title:title,
+            description:description,
+            category:category,
+            instructor:id
+        });
+        await addCourse.save();
+        res.status(200).json({message : "Başarılı bir şekilde kurs eklendi!"},addCourse);
     } catch (error) {
         res.status(500).json({message : error.message})
     }
 };
-const addCourse =async (req,res)=>{
-    
-};
 const updateCourse =async(req,res)=>{
-    
+    try {
+        const {id} =req.params;
+        const {title,description} =req.body;
+        const course = await Course.findById(id);
+        if(!course) return res.status(404).json({message : "kurs bulunamadı!"});
+        if(title) course.title = title;
+        if(description) course.description = description;
+        res.status(200).json({message :"Başarıyla güncellendi", course});
+    } catch (error) {
+        res.status(500).json({message :error.message});
+    }
 };
 const deleteCourse =async (req,res)=>{
-    
+    try {
+        const {id} = req.params;
+        const courseCheck = await Course.findById(id);
+        if(!courseCheck) return res.status(404).json({message : "kurs bulunamadı!"});
+        const deleteCourse = await Course.findByIdAndDelete(id);
+        res.status(200).json({message : "Başarılı bir şekilde silindi", deleteCourse});
+    } catch (error) {
+        res.status(500).json({message : error.message});
+    }
 };
 
 module.exports = {
--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -3,7 +3,7 @@ const router =express.Router();
 const courseController =require("../controller/courseController");
 
 router.get("/",courseController.getCourse);
-router.post("/",courseController.addCourse);
+router.post("/:id",courseController.addCourse);
 router.delete("/:id",courseController.deleteCourse);
 router.put("/:id",courseController.updateCourse);
 
